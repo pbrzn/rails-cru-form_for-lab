5b0127d74385f30de5429a4052b1940988a9387d
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-28 12:21:07 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artists, only: [:index, :new, :create, :show, :edit, :update]
-  resources :genres, only: [:index, :new, :create, :show, :edit, :update]
+  resources :artists, only: [:new, :create, :show, :edit, :update]
+  resources :genres, only: [:new, :create, :show, :edit, :update]
   resources :songs, only: [:index, :new, :create, :show, :edit, :update]
 end
